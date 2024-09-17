146fb685b746a0fd6ca803661aebf8692a2ac338
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2024-09-17 11:11:42 +0200

    added logic to handle different types of HTTP requests like GET and POST

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/":
-      return (
-        res.writeHead(200, { "Content-Type": "text/plain" }),
-        res.end("Welcome to home page")
-      );
-    case "/about":
-      return (
-        res.writeHead(200, { "Content-Type": "text/plain" }),
-        res.end("About us page")
-      );
-    case "/contact":
-      return (
-        res.writeHead(200, { "Content-Type": "text/plain" }),
-        res.end("Contact us page")
-      );
-      default : return res.end("404 error")
+  
+  const successHeaders = { "Content-Type": "text/plain" };
+
+  if (req.method === "GET") {
+    switch (req.url) {
+      case "/":
+        res.writeHead(200, successHeaders);
+        return res.end("Welcome to the home page");
+      case "/about":
+        res.writeHead(200, successHeaders);
+        return res.end("About us page");
+      case "/contact":
+        res.writeHead(200, successHeaders);
+        return res.end("Contact us page");
+      default:
+        res.writeHead(404, successHeaders);
+        return res.end("Error 404: Page not found");
+    }
+  }
+
+  const dummyData = {
+    name: "John Doe",
+    email: "john@doe.com",
+    message: "this is a test message",
+  };
+
+  if (req.method === "POST") {
+    if (req.url === "/contact") {
+      res.writeHead(200, successHeaders);
+      return res.end(`Contact form received: ${JSON.stringify(dummyData)}`);
+    } else {
+      res.writeHead(404, successHeaders);
+      return res.end("Error 404: Page not found");
+    }
   }
 });
 
