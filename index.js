a512b5391d6df0242144a1a5453af2bc278f8948
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2024-09-17 09:03:59 +0200

    basic routes defined, from home to about us and contact us

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const http = require("node:http");
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200);
-  response.end("Welcome, this is a simple node server setup.");
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      return (
+        res.writeHead(200, { "Content-Type": "text/plain" }),
+        res.end("Welcome to home page")
+      );
+    case "/about":
+      return (
+        res.writeHead(200, { "Content-Type": "text/plain" }),
+        res.end("About us page")
+      );
+    case "/contact":
+      return (
+        res.writeHead(200, { "Content-Type": "text/plain" }),
+        res.end("Contact us page")
+      );
+      default : return res.end("404 error")
+  }
 });
 
-server.listen(5000, ()=>{
-    console.log("server is up and running on port 5000")
+server.listen(5000, () => {
+  console.log("server is up and running on port 5000");
 });
-
